cffab3be03e703537b8f52357e9b65e4d75f671b
Tuğrul Hakan Gençtürk <30884233+tuhage@users.noreply.github.com>
2021-10-21 02:49:12 +0300

    Add files via upload

---
+++ codes/selection_sort.js
@@ -0,0 +1,64 @@
+// import visualization libraries {
+    const { Tracer, Array1DTracer, ChartTracer, LogTracer, Randomize, Layout, VerticalLayout } = require('algorithm-visualizer');
+    // }
+    
+    // define tracer variables {
+    const chart = new ChartTracer();
+    const tracer = new Array1DTracer();
+    const logger = new LogTracer();
+    Layout.setRoot(new VerticalLayout([chart, tracer, logger]));
+    const D = Randomize.Array1D({ N: 15 });
+    tracer.set(D);
+    tracer.chart(chart);
+    Tracer.delay();
+    // }
+    
+    // logger {
+    logger.println(`orjinal dizi = [${D.join(', ')}]`);
+    // }
+    for (let i = 0; i < D.length - 1; i++) {
+      let minJ = i;
+      // visualize {
+      tracer.select(i);
+      Tracer.delay();
+      // }
+      for (let j = i + 1; j < D.length; j++) {
+        // visualize {
+        tracer.select(j);
+        Tracer.delay();
+        // }
+        if (D[j] < D[minJ]) {
+          minJ = j;
+          // visualize {
+          tracer.patch(j);
+          Tracer.delay();
+          tracer.depatch(j);
+          // }
+        }
+        // visualize {
+        tracer.deselect(j);
+        // }
+      }
+      if (minJ !== i) {
+        // logger {
+        logger.println(`${D[i]} ile ${D[minJ]} değiştir`);
+        // }
+        const temp = D[i];
+        D[i] = D[minJ];
+        D[minJ] = temp;
+        // visualize {
+        tracer.patch(i, D[i]);
+        tracer.patch(minJ, D[minJ]);
+        Tracer.delay();
+        tracer.depatch(i);
+        tracer.depatch(minJ);
+        // }
+      }
+      // visualize {
+      tracer.deselect(i);
+      // }
+    }
+    // logger {
+    logger.println(`sıralanmış dizi = [${D.join(', ')}]`);
+    // }
+    
